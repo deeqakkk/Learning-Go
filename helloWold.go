a84d68221dea683030e90e312d882b018a3608c4
Deepak Verma <0822deepakverma@gmail.com>
2023-01-03 13:31:47 +0530

    Update helloWold.go

--- helloWold.go
+++ helloWold.go
@@ -1,6 +1,18 @@
 package main
 import ("fmt")
 
+
+func testFun(){
+	var student1 string
+	student1 = "kanak";
+	fmt.Println(student1);
+}
+
 func main() {
 	fmt.Println("Hello Mr. Deepak")
+	var firstVariable  = 1;
+	x := 2;
+	fmt.Println(firstVariable);
+	fmt.Println(x)
+	testFun();
 }
